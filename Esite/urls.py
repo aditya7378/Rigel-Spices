099cd5879bf54f94694c82cbbf1ab11cf0c3e8ef
aditya7378 <adityamahatpure05@gmail.com>
2021-09-24 22:55:17 +0530

    Update urls.py

--- Esite/urls.py
+++ Esite/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    
+    admin.site.site_header  =  "Rigel Spices"  
+    admin.site.site_title  =  "Rigel Spices"
+    admin.site.index_title  =  "Rigel Spices"
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
