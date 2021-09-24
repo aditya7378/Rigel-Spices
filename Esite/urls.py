86a62284ad55121ba9d32117a54af3d54ffbf8ac
aditya7378 <adityamahatpure05@gmail.com>
2021-09-24 22:57:37 +0530

    Update urls.py

--- Esite/urls.py
+++ Esite/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
 
-
+admin.site.site_header  =  "Rigel Spices"
+admin.site.site_title  =  "Rigel Spices"
+admin.site.index_title  =  "Rigel Spices"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
@@ -16,8 +18,6 @@ urlpatterns = [
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    admin.site.site_header  =  "Rigel Spices"  
-    admin.site.site_title  =  "Rigel Spices"
-    admin.site.index_title  =  "Rigel Spices"
+
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
