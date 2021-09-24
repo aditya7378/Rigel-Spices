5a82056ccb1d7f014e17c77d867c1988ecd5d898
aditya7378 <adityamahatpure05@gmail.com>
2021-09-24 22:58:32 +0530

    hello

--- Esite/urls.py
+++ Esite/urls.py
@@ -6,9 +6,7 @@ from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
 
-admin.site.site_header  =  "Rigel Spices"
-admin.site.site_title  =  "Rigel Spices"
-admin.site.index_title  =  "Rigel Spices"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
